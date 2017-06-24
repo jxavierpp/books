44cf3d1703bd7bbee8846b2e3c656bc051b0811e
Xavier Paredes <jxavierpp1@gmail.com>
2017-06-24 11:36:45 -0700

    first commit

---
+++ database/migrations/2017_06_22_222539_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title');
+            $table->integer('pages_count');
+            $table->double('price');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
